cd0490d2301ec01d8bd3b6e87124d4daf69f7bd9
yusuf-kayikci <yusufkayikci@yandex.com>
2018-08-16 02:20:08 +0300

    api url added

---
+++ src/api/apiuri.ts
@@ -0,0 +1,4 @@
+export default class apiUri{
+    public static CURRENCY_API_URI : string = "http://www.doviz.com/api/v1/currencies/all/latest";
+    public static GOLD_API_URI : string = "http://www.doviz.com/api/v1/golds/all/latest";
+}
